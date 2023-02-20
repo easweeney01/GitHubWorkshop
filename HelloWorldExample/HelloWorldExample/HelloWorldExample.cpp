8b626bf44439c1682a63e5205bd547422cf45900
xapresser <115385708+xapresser@users.noreply.github.com>
2023-02-20 15:11:44 -0500

    Update HelloWorldExample.cpp

--- HelloWorldExample/HelloWorldExample/HelloWorldExample.cpp
+++ HelloWorldExample/HelloWorldExample/HelloWorldExample.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "It's me, Emanuel Sweeney\n";
+    std::cout << "It's me, Xander Presser\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
